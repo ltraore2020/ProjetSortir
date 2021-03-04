5db4da9ed83051e988da83246a472543c1498d20
ltraore2020 <lassana.traore2020@campus-eni.fr>
2021-03-04 08:06:20 +0100

    change Home from class to function

--- assets/classes/Home.js
+++ assets/classes/Home.js
@@ -1,126 +1,147 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: '/api'
-});
+export function Home() {
 
-export class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { user: [{ "nom": "" }] }
-        this.getUser();
-    }
+    const api = axios.create({
+        baseURL: '/api'
+    });
 
-    getUser = async () => {
+    const getUser = async () => {
         let data = await api.get('/user')
             .then(({ data }) => data)
             .catch(err => console.log(err, 'error'));
-        this.setState({ user: data })
+        setUser(data);
     };
 
-    componentDidMount() {
-        // fetch('/api/user')
-        //     .then(response => response.json())
-        //     .then(user => {
-        //         this.setState({ data: user });
-        //         console.log(this.state.data);
-        //     })
-        //     .catch(err => console.log(err, 'error'));
-    }
+    const [user, setUser] = useState([{ "pseudo": "Loading..." }]);
 
-    render() {
-        let user = this.state.user;
-        return (
-            <section className="list">
-                <div className="container">
-                    <div className="flex list-info">
-                        <div>
-                            <div>Date du jour : XX/XX/XXXX</div>
-                            <div>Participant : Doe J.</div>
-                        </div>
-                    </div>
+    useEffect(() => {
+        getUser();
+        return () => {
+            // cleanup
+        }
+    });
+
+    return (
+        <section className="list">
+            <div className="container">
+                <div className="flex list-info">
                     <div>
-                        {user[0].pseudo}
+                        <div>Date du jour : XX/XX/XXXX</div>
+                        <div>Participant : {user[0].pseudo}</div>
                     </div>
-                    <div className="title">Filtrer les sorties</div>
-                    <form action="#">
-                        <div className="grid grid-list">
-                            <div className="list-input">
-                                <div>
-                                    Campus: <select name="" id="">
-                                        <option value="">Nantes</option>
-                                        <option value="">Rennes</option>
-                                        <option value="">Niort</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    Contient: <input type="search" name="" id="" />
-                                </div>
-                                <div>
-                                    Entre: <input type="date" name="" id="" />
+                </div>
+                <div className="title">Filtrer les sorties</div>
+                <form action="#">
+                    <div className="grid grid-list">
+                        <div className="list-input">
+                            <div>
+                                Campus: <select name="" id="">
+                                    <option value="">Nantes</option>
+                                    <option value="">Rennes</option>
+                                    <option value="">Niort</option>
+                                </select>
+                            </div>
+                            <div>
+                                Contient: <input type="search" name="" id="" />
+                            </div>
+                            <div>
+                                Entre: <input type="date" name="" id="" />
                             et <input type="date" name="" id="" />
-                                </div>
-                            </div>
-                            <div className="list-check">
-                                <div>
-                                    <input type="checkbox" name="" id="" /> Sorties dont je suis l'organisateur
-                                </div>
-                                <div>
-                                    <input type="checkbox" name="" id="" /> Sorties auxquelles je suis inscrit
-                                </div>
-                                <div>
-                                    <input type="checkbox" name="" id="" /> Sorties auxquelles je suis ne suis pas inscrit
-                                </div>
-                                <div>
-                                    <input type="checkbox" name="" id="" /> Sorties passées
-                                </div>
-                            </div>
-                            <div className="list-search">
-                                <input className="button" type="submit" value="Rechercher" />
                             </div>
                         </div>
-                    </form>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Nom de la sortie</th>
-                                <th>Date de la sortie</th>
-                                <th>Clôture</th>
-                                <th>Inscrits/places</th>
-                                <th>État</th>
-                                <th>Inscrit</th>
-                                <th>Organisateur</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Philo</td>
-                                <td>15/03/2020 23:15</td>
-                                <td>15/03/2020</td>
-                                <td>7/7</td>
-                                <td>En cours</td>
-                                <td>X</td>
-                                <td>Marley B.</td>
-                                <td><a href="/afficher">Afficher</a></td>
-                            </tr>
-                            <tr>
-                                <td>Cuisine</td>
-                                <td>21/03/2020 20:00</td>
-                                <td>20/03/2020</td>
-                                <td>6/10</td>
-                                <td>En création</td>
-                                <td></td>
-                                <td>Doe J.</td>
-                                <td><a href="/modifier">Modifier</a> - Publier</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    {/* <button className="button" >Créer une sortie</button> */}
-                    <a href="/sortie/create" className="button">Créer une sortie </a>
-                </div>
-            </section>
-        );
-    }
+                        <div className="list-check">
+                            <div>
+                                <input type="checkbox" name="" id="" /> Sorties dont je suis l'organisateur
+                                </div>
+                            <div>
+                                <input type="checkbox" name="" id="" /> Sorties auxquelles je suis inscrit
+                                </div>
+                            <div>
+                                <input type="checkbox" name="" id="" /> Sorties auxquelles je suis ne suis pas inscrit
+                                </div>
+                            <div>
+                                <input type="checkbox" name="" id="" /> Sorties passées
+                                </div>
+                        </div>
+                        <div className="list-search">
+                            <input className="button" type="submit" value="Rechercher" />
+                        </div>
+                    </div>
+                </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nom de la sortie</th>
+                            <th>Date de la sortie</th>
+                            <th>Clôture</th>
+                            <th>Inscrits/places</th>
+                            <th>État</th>
+                            <th>Inscrit</th>
+                            <th>Organisateur</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Philo</td>
+                            <td>15/03/2020 23:15</td>
+                            <td>15/03/2020</td>
+                            <td>7/7</td>
+                            <td>En cours</td>
+                            <td>X</td>
+                            <td>Marley B.</td>
+                            <td><a href="/afficher">Afficher</a></td>
+                        </tr>
+                        <tr>
+                            <td>Cuisine</td>
+                            <td>21/03/2020 20:00</td>
+                            <td>20/03/2020</td>
+                            <td>6/10</td>
+                            <td>En création</td>
+                            <td></td>
+                            <td>Doe J.</td>
+                            <td><a href="/modifier">Modifier</a> - Publier</td>
+                        </tr>
+                    </tbody>
+                </table>
+                {/* <button className="button" >Créer une sortie</button> */}
+                <a href="/sortie/create" className="button">Créer une sortie </a>
+            </div>
+        </section>
+    )
 }
+
+
+// class Home extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { user: [{ "nom": "" }] }
+//         this.getUser();
+//     }
+
+//     getUser = async () => {
+//         let data = await api.get('/user')
+//             .then(({ data }) => data)
+//             .catch(err => console.log(err, 'error'));
+//         this.setState({ user: data })
+//     };
+
+//     componentDidMount() {
+//         // fetch('/api/user')
+//         //     .then(response => response.json())
+//         //     .then(user => {
+//         //         this.setState({ data: user });
+//         //         console.log(this.state.data);
+//         //     })
+//         //     .catch(err => console.log(err, 'error'));
+//     }
+
+//     render() {
+//         let user = this.state.user;
+//         return (
+
+//         );
+//     }
+// }
